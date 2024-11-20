d750e4a2c8d3dda42c34c74502a654259cad8b57
Keshav Madhav <keshav2552003@gmail.com>
2024-11-20 15:33:30 +0530

    Enabled cors for all domains

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
 from RAG.api.api import router as RAGAPI_router
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+) 
 
 @app.get("/")
 async def health():
