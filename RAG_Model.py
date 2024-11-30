91fc9162c0e50ba519df1d944a7b0f5677acc79c
Banshee <manasbisht2507@gmail.com>
2024-11-30 15:09:53 +0530

    added image txt extraction with multi file extension support

--- RAG_Model.py
+++ RAG_Model.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-os.environ["OPENAI_API_KEY"] = "sk-proj-5lSghsl91phIO5GDerGCT3BlbkFJqAE76qU7huuNO3BChla8"
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 class Rag_Model:
 
